34595572ff9a966007658c89dd3dea73da744de6
Darien Khoo <darienkhoo@Unknown.domain>
2026-08-11 13:43:39 -0700

    new work

--- public/main.js
+++ public/main.js
@@ -1,3 +1,27 @@
 //FoodData central API key (U.S agricultural) 
 const apiKey = 'HC5yHN6QbG53eqJW8sUkqwrJRLp3zi1wpfGAbFKh';
 
+async function getFoodSearch(food){
+    // const searchEndpoint = `https://api.nal.usda.gov/fdc/v1/foods/search?query=${food}&dataType=Branded&pageSize=5&pageNumber=1&sortBy=dataType.keyword&sortOrder=asc&brandOwner=Kar%20Nut%20Products%20Company&api_key=HC5yHN6QbG53eqJW8sUkqwrJRLp3zi1wpfGAbFKh`;
+
+    const searchEndpoint = '/netlify/functions/askNutri';
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify({
+            message: food
+        })
+    };
+
+    try {
+         let response = await fetch(searchEndpoint, options);
+         //console.log(response);
+         return await response.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//getFoodSearch('rice');
+console.log(getFoodSearch('brown rice'));
+
