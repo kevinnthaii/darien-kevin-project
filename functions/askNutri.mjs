86dcc46662e7f66b64bb750177daf42c7937dcdb
Kevin Thai <kevinthai72008@gmail.com>
2026-08-11 13:32:33 -0700

    set up netlify complete

--- functions/askNutri.mjs
+++ functions/askNutri.mjs
@@ -39,22 +39,3 @@ export default async function askNutri(request) {
 
 
 
-async function getFoodSearch(food) {
-    //let
-    const searchEndpoint = https://api.nal.usda.gov/fdc/v1/foods/search?query=${food}&dataType=Branded&pageSize=5&pageNumber=1&sortBy=dataType.keyword&sortOrder=asc&brandOwner=Kar%20Nut%20Products%20Company&api_key=HC5yHN6QbG53eqJW8sUkqwrJRLp3zi1wpfGAbFKh;
-
-    const options = {
-        method: 'GET'
-    };
-
-    try {
-        let response = await fetch(searchEndpoint, options);
-        console.log(response);
-        return await response.json();
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-//getFoodSearch('rice');
-console.log(getFoodSearch('brown rice'));
